dabe22530bbf7d7b62935e62ba62512260df1be9
John Stark <jhnstrk@gmail.com>
2022-12-15 08:34:49 +0100

    Add lib and backend, make workspaces

---
+++ frontend/src/main.rs
@@ -0,0 +1,25 @@
+use yew::prelude::*;
+
+#[function_component(App)]
+fn app() -> Html {
+    html! {
+        <>
+            <h1>{ "RustConf Explorer" }</h1>
+            <div>
+                <h3>{"Videos to watch"}</h3>
+                <p>{ "John Doe: Building and breaking things" }</p>
+                <p>{ "Jane Smith: The development process" }</p>
+                <p>{ "Matt Miller: The Web 7.0" }</p>
+                <p>{ "Tom Jerry: Mouseless development" }</p>
+            </div>
+            <div>
+                <h3>{ "John Doe: Building and breaking things" }</h3>
+                <img src="https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder" alt="video thumbnail" />
+            </div>
+        </>
+    }
+}
+
+fn main() {
+    yew::Renderer::<App>::new().render();
+}
